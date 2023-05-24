1aad4c0ff4ff830bf13246eef5802b5b3e0af29c
Los-wapos-xd <131193171+Los-wapos-xd@users.noreply.github.com>
2023-05-24 08:57:38 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,35 @@
 input.onButtonPressed(Button.A, function () {
-    basic.pause(7000)
-    mensaje_alamasigan = 0
+    maqueen.servoRun(maqueen.Servos.S2, 0)
+    mensaje_alamasigan = 180
+    basic.pause(1000)
+    if (mensaje_alamasigan == 180) {
+        while (mensaje_alamasigan == 180) {
+            if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0) {
+                maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+            } else {
+                if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1) {
+                    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+                    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+                    if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1) {
+                        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+                        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+                    }
+                } else {
+                    if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0) {
+                        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+                        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+                        if ((0 as any) == (1 as any) && (0 as any) == (1 as any)) {
+                            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+                            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+                        }
+                    }
+                }
+            }
+        }
+    } else {
+    	
+    }
 })
 let mensaje_alamasigan = 0
-maqueen.servoRun(maqueen.Servos.S2, 180)
+maqueen.servoRun(maqueen.Servos.S2, 100)
 mensaje_alamasigan = 0
