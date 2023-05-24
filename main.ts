4c0fec0de10c61759386675fccf021ae3bd3125d
Los-wapos-xd <131193171+Los-wapos-xd@users.noreply.github.com>
2023-05-24 08:48:01 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
-basic.forever(function () {
-    basic.showString("XDDDD")
+input.onButtonPressed(Button.A, function () {
+    basic.pause(7000)
+    mensaje_alamasigan = 0
 })
+let mensaje_alamasigan = 0
+maqueen.servoRun(maqueen.Servos.S2, 180)
+mensaje_alamasigan = 0
